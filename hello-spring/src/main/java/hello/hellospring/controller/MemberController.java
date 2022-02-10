e5674d8493c81c96a79aca3778d25a8979670327
PPPosik <eoanswkgksk@gmail.com>
2022-02-10 14:29:17 +0900

    Update spring bean manual configuration example

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+    // field DI
+//    @Autowired private MemberService memberService;
     private final MemberService memberService;
 
+    // constructor DI
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    // setter DI
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 }
