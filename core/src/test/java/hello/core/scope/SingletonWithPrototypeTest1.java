45a4685bd29bfc73e3f95aca4e956e3c9653bb0d
PPPosik <eoanswkgksk@gmail.com>
2022-03-04 08:54:24 +0900

    Add singleton with prototype

---
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,74 @@
+package hello.core.scope;
+
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonWithPrototypeTest1 {
+    @Test
+    public void prototypeFind() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean bean1 = applicationContext.getBean(PrototypeBean.class);
+        bean1.addCnt();
+        assertThat(bean1.getCnt()).isEqualTo(1);
+
+        PrototypeBean bean2 = applicationContext.getBean(PrototypeBean.class);
+        bean2.addCnt();
+        assertThat(bean2.getCnt()).isEqualTo(1);
+    }
+
+    @Test
+    public void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean client1 = applicationContext.getBean(ClientBean.class);
+        int cnt1 = client1.logic();
+        assertThat(cnt1).isEqualTo(1);
+
+        ClientBean client2 = applicationContext.getBean(ClientBean.class);
+        int cnt2 = client2.logic();
+        assertThat(cnt2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    @RequiredArgsConstructor
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        public int logic() {
+            prototypeBean.addCnt();
+            return prototypeBean.getCnt();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int cnt = 0;
+
+        public void addCnt() {
+            cnt++;
+        }
+
+        public int getCnt() {
+            return cnt;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy" + this);
+        }
+    }
+}
