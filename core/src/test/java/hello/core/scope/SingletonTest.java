b71b8e7bed0e5b611f5c8a8bbcd4224f9bcc5923
PPPosik <eoanswkgksk@gmail.com>
2022-03-03 18:25:57 +0900

    Add prototype bean

---
+++ core/src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,37 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+    @Test
+    public void singletonBeanFind() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean bean1 = applicationContext.getBean(SingletonBean.class);
+        SingletonBean bean2 = applicationContext.getBean(SingletonBean.class);
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+
+        Assertions.assertThat(bean1).isSameAs(bean2);
+        applicationContext.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
