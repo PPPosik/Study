443ec89ee4307b7a69483e00098cf096231eddbd
PPPosik <eoanswkgksk@gmail.com>
2022-04-04 16:22:44 +0900

    Extract validation

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -25,6 +25,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -135,7 +136,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         log.info("bindingResult = {}", bindingResult.getObjectName());
         log.info("bindingResult = {}", bindingResult.getTarget());
@@ -154,6 +155,26 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        log.info("bindingResult = {}", bindingResult.getObjectName());
+        log.info("bindingResult = {}", bindingResult.getTarget());
+
+        if (itemValidator.supports(item.getClass())) {
+            itemValidator.validate(item, bindingResult);
         }
 
         if (bindingResult.hasErrors()) {
