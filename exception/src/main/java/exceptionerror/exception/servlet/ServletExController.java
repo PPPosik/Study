35370fb14b3c49afaf50865568abe5b82a4cb7d5
PPPosik <eoanswkgksk@gmail.com>
2022-04-10 13:34:04 +0900

    Add spring default error page

--- exception/src/main/java/exceptionerror/exception/servlet/ServletExController.java
+++ exception/src/main/java/exceptionerror/exception/servlet/ServletExController.java
@@ -15,6 +15,11 @@ public class ServletExController {
         throw new RuntimeException("exception");
     }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, "400 error");
+    }
+
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(HttpServletResponse.SC_NOT_FOUND, "404 error");
