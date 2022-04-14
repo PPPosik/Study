b144c6b137e6e79b3fce1622cb0e074352558db3
PPPosik <eoanswkgksk@gmail.com>
2022-04-14 09:54:54 +0900

    Move directory

--- advanced/src/main/java/hello/advanced/app/v0/OrderControllerV0.java
+++ advanced/src/main/java/hello/advanced/app/v0/OrderControllerV0.java
@@ -1,8 +1,7 @@
-package hello.advanced.v0;
+package hello.advanced.app.v0;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
