a9f1268507d3ee774b238c837d294174ccc18c47
PPPosik <eoanswkgksk@gmail.com>
2022-04-15 10:14:47 +0900

    Apply field sync trace

---
+++ advanced/src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v3")
+public class OrderControllerV3 {
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/request")
+    public String request(String itemId) {
+        TraceStatus status = trace.begin("OrderController.request()");
+        try {
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
