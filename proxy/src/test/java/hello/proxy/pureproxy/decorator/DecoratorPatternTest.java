f0a8d625f00b56b28c4a6a72d82d860d86d5e6fb
PPPosik <eoanswkgksk@gmail.com>
2022-04-18 12:45:52 +0900

    Add decorator exam3

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.proxy.pureproxy.decorator;
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +18,15 @@ public class DecoratorPatternTest {
         Component realComponent = new RealComponent();
         Component decorator = new MessageDecorator(realComponent);
         DecoratorPatternClient client = new DecoratorPatternClient(decorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component decorator1 = new MessageDecorator(realComponent);
+        Component decorator2 = new TimeDecorator(decorator1);
+        DecoratorPatternClient client = new DecoratorPatternClient(decorator2);
         client.execute();
     }
 }
---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/code/TimeDecorator.java
@@ -0,0 +1,22 @@
+package hello.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeDecorator implements Component {
+    private Component component;
+
+    public TimeDecorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String operation() {
+        log.info("call TimeDecorator");
+        long startTime = System.currentTimeMillis();
+        String result = component.operation();
+        long endTime = System.currentTimeMillis();
+        log.info("end TimeDecorator time={}", endTime - startTime);
+        return result;
+    }
+}
