57ba78e3c629a39f5237e9f6f03b715b6c4d9109
PPPosik <eoanswkgksk@gmail.com>
2022-04-28 14:22:41 +0900

    Add bean post processor exam

---
+++ proxy/src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,68 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+public class BasicTest {
+    @Test
+    void basicConfig() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
+
+        B b = applicationContext.getBean("beanA", B.class);
+        b.helloB();
+
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(A.class));
+    }
+
+    @Configuration
+    static class BeanPostProcessorConfig {
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+
+        @Bean
+        public AToBPostProcessor aToBPostProcessor() {
+            return new AToBPostProcessor();
+        }
+    }
+
+    static class A {
+        public void hellA() {
+            log.info("Hello A");
+        }
+    }
+
+    static class B {
+        public void helloB() {
+            log.info("HelloB");
+        }
+    }
+
+    static class AToBPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+            return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+        }
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={}, bean={}", beanName, bean);
+
+            if (bean instanceof A) {
+                return new B();
+            }
+
+            return bean;
+        }
+    }
+
+}
