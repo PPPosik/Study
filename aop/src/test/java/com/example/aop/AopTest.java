aac9f757b294ceacbd8e9d7c6ac0dcf002c8b899
PPPosik <eoanswkgksk@gmail.com>
2022-05-10 14:48:23 +0900

    Add Pointcuts

--- aop/src/test/java/com/example/aop/AopTest.java
+++ aop/src/test/java/com/example/aop/AopTest.java
@@ -5,6 +5,7 @@ import com.example.aop.order.OrderService;
 import com.example.aop.order.aop.AspectV1;
 import com.example.aop.order.aop.AspectV2;
 import com.example.aop.order.aop.AspectV3;
+import com.example.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopTest {
     @Autowired
     OrderService orderService;
