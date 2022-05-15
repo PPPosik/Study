272e29a08b219738683c63673efc06e7ff9e01f5
PPPosik <eoanswkgksk@gmail.com>
2022-05-15 14:33:12 +0900

    Add exam log aop

--- aop/src/main/java/com/example/aop/exam/ExamRepository.java
+++ aop/src/main/java/com/example/aop/exam/ExamRepository.java
@@ -1,11 +1,13 @@
 package com.example.aop.exam;
 
+import com.example.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ExamRepository {
     private static int seq = 0;
 
+    @Trace
     public String save(String itemId) {
         seq++;
         if (seq % 5 == 0) {
