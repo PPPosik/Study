0d81ef39726b4a00a8c558320ea2fc948a8f4d2d
PPPosik <eoanswkgksk@gmail.com>
2022-05-15 14:26:43 +0900

    Add exam

---
+++ aop/src/test/java/com/example/aop/exam/ExamTest.java
@@ -0,0 +1,21 @@
+package com.example.aop.exam;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class ExamTest {
+    @Autowired
+    ExamService examService;
+
+    @Test
+    void test() {
+        for (int i = 1; i <= 5; i++) {
+            log.info("client request={}", i);
+            examService.request("data" + i);
+        }
+    }
+}
