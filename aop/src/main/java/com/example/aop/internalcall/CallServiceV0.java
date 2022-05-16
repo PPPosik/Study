a3d9fed5bc320e8d7a12fb74a301e3df11024b0d
PPPosik <eoanswkgksk@gmail.com>
2022-05-16 13:59:52 +0900

    Add proxy internal call problem exam

---
+++ aop/src/main/java/com/example/aop/internalcall/CallServiceV0.java
@@ -0,0 +1,17 @@
+package com.example.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class CallServiceV0 {
+    public void external() {
+        log.info("call external");
+        internal();
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
