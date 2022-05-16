515731c3859c6f3fbbbc9ab53101aa0d4271fcee
PPPosik <eoanswkgksk@gmail.com>
2022-05-16 15:05:44 +0900

    Add proxy internal call solution1

---
+++ aop/src/test/java/com/example/aop/internalcall/CallServiceV1Test.java
@@ -0,0 +1,21 @@
+package com.example.aop.internalcall;
+
+import com.example.aop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+@Import(CallLogAspect.class)
+public class CallServiceV1Test {
+    @Autowired
+    CallServiceV1 callServiceV1;
+
+    @Test
+    void external() {
+        callServiceV1.external();
+    }
+}
