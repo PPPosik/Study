7d73047f289bdebb0ec5667cec1f531807c51b2d
PPPosik <eoanswkgksk@gmail.com>
2022-05-20 18:27:08 +0900

    First commit

---
+++ toby1/src/main/java/book/toby1/Toby1Application.java
@@ -0,0 +1,13 @@
+package book.toby1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toby1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toby1Application.class, args);
+	}
+
+}
---
+++ toby1/src/test/java/book/toby1/Toby1ApplicationTests.java
@@ -0,0 +1,13 @@
+package book.toby1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toby1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
