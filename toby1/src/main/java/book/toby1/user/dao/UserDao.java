5149e0866cae4f97c525f7c0361cdf00cda5a6d7
PPPosik <eoanswkgksk@gmail.com>
2022-06-12 00:54:45 +0900

    Add 3.3 apply add strategy pattern

--- toby1/src/main/java/book/toby1/user/dao/UserDao.java
+++ toby1/src/main/java/book/toby1/user/dao/UserDao.java
@@ -21,17 +21,8 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
-        Connection connection = dataSource.getConnection();
-        PreparedStatement ps = connection.prepareStatement("insert into users(id, name, password) values (?, ?, ?)");
-
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        connection.close();
+        StatementStrategy strategy = new AddStatement(user);
+        jdbcContextWithStatementStrategy(strategy);
     }
 
     public User get(String id) throws SQLException {
