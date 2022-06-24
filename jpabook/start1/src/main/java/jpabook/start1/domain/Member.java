02f82af36e01a6ad0e5fd03d315aaf00a15f1a2e
PPPosik <eoanswkgksk@gmail.com>
2022-06-24 15:10:51 +0900

    Add 4.6~4.7 entity mapping

--- jpabook/start1/src/main/java/jpabook/start1/domain/Member.java
+++ jpabook/start1/src/main/java/jpabook/start1/domain/Member.java
@@ -6,9 +6,12 @@ import java.util.Date;
 @Entity
 //@org.hibernate.annotations.DynamicUpdate
 @Table(name = "member", uniqueConstraints = {@UniqueConstraint(name = "NAME_AGE_UNIQUE", columnNames = {"name", "age"})})
+@Access(AccessType.FIELD) // 필드 직접 접근, private 이어도 접근할 수 있음
+// @Access(AccessType.PROPERTY) // getter 접근자를 사용해서 접근
 public class Member {
     @Id
     @Column(name = "id", length = 100) // Specified key was too long; max key length is 1000 bytes, default 255에서 인코딩 방식에 따라 255 * n bytes로 계산됨
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
 
     @Column(name = "name", nullable = false, length = 10)
@@ -17,7 +20,7 @@ public class Member {
     // 기본 매핑 사용
     private Integer age;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING) // EnumType.ORDINAL
     private RoleType roleType;
 
     @Temporal(TemporalType.TIMESTAMP)
