bae0f75b9d88e4b5a0fa9ff901691c025d571c64
PPPosik <eoanswkgksk@gmail.com>
2023-07-03 22:42:57 +0900

    Add toby2

---
+++ Spring/Toby/toby2/src/main/kotlin/book/toby2/Toby2Application.kt
@@ -0,0 +1,11 @@
+package book.toby2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Toby2Application
+
+fun main(args: Array<String>) {
+	runApplication<Toby2Application>(*args)
+}
---
+++ Spring/Toby/toby2/src/test/kotlin/book/toby2/Toby2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package book.toby2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Toby2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
